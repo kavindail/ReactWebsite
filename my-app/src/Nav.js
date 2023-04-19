8a7e9cc7d2b5f1883513684b535d27652c2ea1af
Kavin <kavindail17@gmail.com>
2023-04-18 18:43:15 -0700

    first commit for my react website!

---
+++ my-app/src/Nav.js
@@ -0,0 +1,34 @@
+import './Nav.css';
+
+
+function Nav() {
+  
+
+    return (
+<div id = "Nav">
+            
+<a  target="_blank" class="logo" >Kavin Dail</a>
+<br></br>
+
+<div class = "ul">
+    <div class = "li" ><a href="#Landing">Home</a></div>
+    <div class = "li"><a href="#about">About</a></div>
+    <div class = "li"><a href="#projects">Projects</a></div>
+    <div class = "li"><a href="#ending">Contact</a></div>
+
+</div>
+
+
+
+</div>
+
+
+  );
+}
+window.addEventListener("scroll", () => {
+	let header = document.querySelector("#Nav");
+	header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+
+export default Nav;
