12412da138e5c758dc3dc41cf37a6d540bd40c81
Kavin <kavindail17@gmail.com>
2023-04-23 11:55:48 -0700

    added about section unfinished still needs spinning text cube

--- my-app/src/About.js
+++ my-app/src/About.js
@@ -5,29 +5,30 @@ function About() {
 
 
   return (
-<div id = "About">
+<div id = "about">
+
 <div>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-
-   <h1>My Skills</h1>
-   <br></br>
-   <br></br>
-
-   <p>I have been into computing for a long while and recently picked up coding so that I can finally create the projects and apps I would personally want to use.
     <br></br>
     <br></br>
-    For the past few months, I've been at BCIT doing the CST program learning the skills neccesary to make projects I'm proud of. I want to learn more so I can make programs that are useful to everyone.
+    <br></br>
+    <br></br>
+
+<h1>My Skills</h1>
 <br></br>
 <br></br>
- I hope to land a full stack developer position and am eagerly awaiting the future!
+
+<p>I have been into computing for a long while and recently picked up coding so that I can finally create the projects and apps I would personally want to use.
+<br></br>
+<br></br>
+For the past few months, I've been at BCIT doing the CST program learning the skills neccesary to make projects I'm proud of. I want to learn more so I can make programs that are useful to everyone.
+<br></br>
+<br></br>
+I hope to land a full stack developer position and am eagerly awaiting the future!
 
 
 
 
-   </p>
+</p>
 
 
 </div>
@@ -35,12 +36,9 @@ function About() {
 
 
 
-  
-    
-
 </div>
 
-    );
+      );
     
    
  
