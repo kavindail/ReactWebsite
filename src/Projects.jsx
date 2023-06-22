d2f177f24f9c20959110a083dbda18f9196db49b
Kavin Dail <kavindail17@gmail.com>
2023-06-21 18:25:24 -0700

    media queries and finished projects section

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -5,58 +5,70 @@ const Projects = () => {
 
 
 return(
-<div id="projects"> 
+<div className="projects"> 
+  <h1>Projects</h1>
+  <div className="page-content">
+    <div class="card">
+      <div class="content">
+        <h2 class="title">PacMan + Snake</h2>
+        <p class="copy">Check out all of these gorgeous mountain trips with beautiful views of, you guessed it, the mountains</p>
+        <div class="pill-buttons">
+          <button> Java</button>
+          <button> JS</button>
+          <button> Python</button>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="content">
+        <h2 class="title">SuccessWeb</h2>
+        <p class="copy">Plan your next beach trip with these fabulous destinations</p>
+        <div class="pill-buttons">
+          <button>Scrap.py</button>
+          <button> JS</button>
+          <button> Python</button>
+          <button> Angular</button>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="content">
+        <h2 class="title">CoverAI</h2>
+        <p class="copy">It's the desert you've always dreamed of</p>
+        <div class="pill-buttons">
+          <button> Java</button>
+          <button> JS</button>
+          <button> Python</button>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="content">
+        <h2 class="title">TBD</h2>
+        <p class="copy">TBD</p>
+        <div class="pill-buttons">
+        </div>
+      </div>
+    </div>
 
-    <h1>Projects</h1>
- 
-  <main class="page-content">
-    <div>
-      <div class="card">
-        <div class="content">
-          <h2 class="title">PacMan + Snake</h2>
-          <p class="copy">Check out all of these gorgeous mountain trips with beautiful views of, you guessed it, the mountains</p>
-          <div class="pill-buttons">
-<button> Java</button>
-<button> JS</button>
-<button> Python</button>
-</div>
-        </div>
-      </div>
-      <div class="card">
-        <div class="content">
-          <h2 class="title">SuccessWeb</h2>
-          <p class="copy">Plan your next beach trip with these fabulous destinations</p>
-          <div class="pill-buttons">
-<button>Scrap.py</button>
-<button> JS</button>
-<button> Python</button>
-<button> Angular</button>
-</div>
+    <div class="card">
+      <div class="content">
+        <h2 class="title">TBD</h2>
+        <p class="copy">TBD</p>
+        <div class="pill-buttons">
         </div>
       </div>
     </div>
-    <div>
-      <div class="card">
-        <div class="content">
-          <h2 class="title">CoverAI</h2>
-          <p class="copy">It's the desert you've always dreamed of</p>
-          <div class="pill-buttons">
-<button> Java</button>
-<button> JS</button>
-<button> Python</button>
-</div>
-        </div>
-      </div>
-      <div class="card">
-        <div class="content">
-          <h2 class="title">TBD</h2>
-          <p class="copy">TBD</p>
-          <div class="pill-buttons">
-</div>
+
+    <div class="card">
+      <div class="content">
+        <h2 class="title">TBD</h2>
+        <p class="copy">TBD</p>
+        <div class="pill-buttons">
         </div>
       </div>
     </div>
-  </main>
+  </div>
 </div>
 
 
